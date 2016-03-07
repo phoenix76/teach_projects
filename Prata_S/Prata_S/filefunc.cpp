6ca2d703385dec33898e1a3825f7feaa1c8b7f49
Dmitry Yakovlev <djokerxx2013@mail.ru>
2016-03-07 14:30:17 +0400

    filefunc_pr

---
+++ Prata_S/Prata_S/filefunc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+void file_it(ostream &os, double fo, const double fe[], int n);
+const int LIMIT = 5;
+int main()
+{
+	
+}
